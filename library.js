90c6c0828cfec54a6793294fbdc4f15f4a24e256
Abosede Salami <sabosede65.sa@gmail.com>
2021-02-19 12:51:24 +0100

    first commit

---
+++ library.js
@@ -0,0 +1,20 @@
+function Calc (a,b){
+    this.firstValue = a;
+    this.secondValue = b;
+}
+
+Calc.prototype.addNum = function () {
+    return this.firstValue + this.secondValue;
+}
+
+Calc.prototype.subtract = function () {
+    return this.firstValue - this.secondValue;
+}
+
+Calc.prototype.multiply = function () {
+    return this.firstValue * this.secondValue;
+}
+
+Calc.prototype.divide = function () {
+    return this.firstValue / this.secondValue;
+}
